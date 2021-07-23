191bef1aca7e9fd62b9e0f0f03eac727e10ea706
Melvin Tu <melvinto@gmail.com>
2021-07-23 15:17:07 +0800

    use ns for wireguard

--- plugins/interface/wireguard_intf_plugin.js
+++ plugins/interface/wireguard_intf_plugin.js
@@ -25,6 +25,13 @@ const routing = require('../../util/routing.js');
 const util = require('../../util/util.js');
 const {Address4, Address6} = require('ip-address');
 
+const nsName = "ns-wg";
+const brName = "br-wg";
+const vethHost = "veth-wg-br";
+const vethNS = "veth-wg";
+const vethNSIP = "172.31.100.2/30";
+const vethHostIP = "172.31.100.1/30";
+const vethSubnet = "172.31.100.0/30";
 
 const Promise = require('bluebird');
 Promise.promisifyAll(fs);
@@ -43,20 +50,16 @@ class WireguardInterfacePlugin extends InterfaceBasePlugin {
     await fs.unlinkAsync(this._getInterfaceConfPath()).catch((err) => {});
     if (this.networkConfig.listenPort) {
 
-      const firewallPort = this.getFirewallPort();
       const listenPort = this.networkConfig.listenPort;
 
-      // port redirect, this is to fix the conflict between wireguard binding all interfaces with dual wan load balancing
-      if(firewallPort !== listenPort) {
-        const statement = `-w -t nat -D FR_WIREGUARD_DEFAULT -p udp --dport ${firewallPort} -j REDIRECT --to-port ${listenPort}`;
-        await exec(util.wrapIptables(`sudo iptables ${statement}`));
-        await exec(util.wrapIptables(`sudo ip6tables ${statement}`));
-      } else {
-        await exec(util.wrapIptables(`sudo iptables -w -D FR_WIREGUARD -p udp --dport ${firewallPort} -j ACCEPT`)).catch((err) => {});
-        await exec(util.wrapIptables(`sudo ip6tables -w -D FR_WIREGUARD -p udp --dport ${firewallPort} -j ACCEPT`)).catch((err) => {});
-        await exec(util.wrapIptables(`sudo iptables -w -t nat -D FR_WIREGUARD -p udp --dport ${firewallPort} -j ACCEPT`)).catch((err) => {});
-        await exec(util.wrapIptables(`sudo ip6tables -w -t nat -D FR_WIREGUARD -p udp --dport ${firewallPort} -j ACCEPT`)).catch((err) => {});
+      if(this.networkConfig.nsMode === true) {
+        this.destroyNSEnvironment();
       }
+
+      await exec(util.wrapIptables(`sudo iptables -w -D FR_WIREGUARD -p udp --dport ${firewallPort} -j ACCEPT`)).catch((err) => {});
+      await exec(util.wrapIptables(`sudo ip6tables -w -D FR_WIREGUARD -p udp --dport ${firewallPort} -j ACCEPT`)).catch((err) => {});
+      await exec(util.wrapIptables(`sudo iptables -w -t nat -D FR_WIREGUARD -p udp --dport ${firewallPort} -j ACCEPT`)).catch((err) => {});
+      await exec(util.wrapIptables(`sudo ip6tables -w -t nat -D FR_WIREGUARD -p udp --dport ${firewallPort} -j ACCEPT`)).catch((err) => {});
       
     }
   }
@@ -67,6 +70,34 @@ class WireguardInterfacePlugin extends InterfaceBasePlugin {
 
   getFirewallPort() {
     return this.networkConfig.firewallPort || this.networkConfig.listenPort;
+  }
+
+  async setupNSEnvironment() {
+    const reservedSubnet = "172.31.100.0/24";
+
+    // create namespace
+    await exec(`sudo ip netns add ${nsName}`);
+
+    // veth
+    await exec(`sudo ip link add ${vethNS} type veth peer name ${vethHost}`);
+    await exec(`sudo ip link set ${vethNS} netns ${nsName}`);
+    await exec(`sudo ip -n ${nsName} addr add ${vethNSIP} dev ${vethNS}`);
+    await exec(`sudo ip -n ${nsName} link set ${vethNS} up`);
+
+    // bridge
+    await exec(`sudo ip link add ${brName} type bridge`);
+    await exec(`sudo ip link set ${vethHost} master ${brName}`);
+    await exec(`sudo ip link set ${vethHost} up`);
+    await exec(`sudo ip addr add ${brName} dev ${vethHost}`);    
+
+    await exec(`sudo ip netns exec ${nsName} ip route add default via ${vethNSIP} dev ${vethNS}`);
+    await exec(`sudo ip netns exec ${nsName} iptables -t nat -A POSTROUTING -o ${vethNS} -j MASQUERADE`);
+  }
+
+  async destroyNSEnvironment() {
+    await exec(`sudo ip netns del ${nsName}`);
+    await exec(`sudo ip link set ${brName} down`);
+    await exec(`sudo ip link del ${brName} type bridge`);    
   }
   
   async createInterface() {
@@ -80,21 +111,16 @@ class WireguardInterfacePlugin extends InterfaceBasePlugin {
       entries.push(`ListenPort = ${this.networkConfig.listenPort}`);
       if (this.networkConfig.enabled) {
 
-        const firewallPort = this.getFirewallPort();
         const listenPort = this.networkConfig.listenPort;
 
-        // port redirect, this is to fix the conflict between wireguard binding all interfaces with dual wan load balancing
-        if(firewallPort !== listenPort) {
-          const statement = `-w -t nat -A FR_WIREGUARD_DEFAULT -p udp --dport ${firewallPort} -j REDIRECT --to-port ${listenPort}`;
-          await exec(util.wrapIptables(`sudo iptables ${statement}`));
-          await exec(util.wrapIptables(`sudo ip6tables ${statement}`));
-        } else {
-          await exec(util.wrapIptables(`sudo iptables -w -A FR_WIREGUARD -p udp --dport ${firewallPort} -j ACCEPT`)).catch((err) => {});
-          await exec(util.wrapIptables(`sudo ip6tables -w -A FR_WIREGUARD -p udp --dport ${firewallPort} -j ACCEPT`)).catch((err) => {});
-          await exec(util.wrapIptables(`sudo iptables -w -t nat -A FR_WIREGUARD -p udp --dport ${firewallPort} -j ACCEPT`)).catch((err) => {});
-          await exec(util.wrapIptables(`sudo ip6tables -w -t nat -A FR_WIREGUARD -p udp --dport ${firewallPort} -j ACCEPT`)).catch((err) => {});          
+        if(this.networkConfig.nsMode === true) {
+          this.setupNSEnvironment();
         }
 
+        await exec(util.wrapIptables(`sudo iptables -w -A FR_WIREGUARD -p udp --dport ${firewallPort} -j ACCEPT`)).catch((err) => {});
+        await exec(util.wrapIptables(`sudo ip6tables -w -A FR_WIREGUARD -p udp --dport ${firewallPort} -j ACCEPT`)).catch((err) => {});
+        await exec(util.wrapIptables(`sudo iptables -w -t nat -A FR_WIREGUARD -p udp --dport ${firewallPort} -j ACCEPT`)).catch((err) => {});
+        await exec(util.wrapIptables(`sudo ip6tables -w -t nat -A FR_WIREGUARD -p udp --dport ${firewallPort} -j ACCEPT`)).catch((err) => {});         
       }
     }
     entries.push('\n');
@@ -129,10 +155,17 @@ class WireguardInterfacePlugin extends InterfaceBasePlugin {
         if (peer.allowedIPs) {
           for (const allowedIP of peer.allowedIPs) {
             if (this.isLAN()) {
-              // add peer networks to wan_routable and lan_routable
-              await routing.addRouteToTable(allowedIP, null, this.name, routing.RT_LAN_ROUTABLE, null, new Address4(allowedIP).isValid() ? 4 : 6).catch((err) => {});
-              await routing.addRouteToTable(allowedIP, null, this.name, routing.RT_WAN_ROUTABLE, null, new Address4(allowedIP).isValid() ? 4 : 6).catch((err) => {});
-            }
+
+              if(this.networkConfig.nsMode === true) {
+                await routing.addRouteToTable(vethSubnet, null, vethHost, routing.RT_LAN_ROUTABLE, null, 4).catch((err) => {});
+                await routing.addRouteToTable(vethSubnet, null, vethHost, routing.RT_WAN_ROUTABLE, null, 4).catch((err) => {});
+              } else {
+                // add peer networks to wan_routable and lan_routable
+                await routing.addRouteToTable(allowedIP, null, this.name, routing.RT_LAN_ROUTABLE, null, new Address4(allowedIP).isValid() ? 4 : 6).catch((err) => {});
+                await routing.addRouteToTable(allowedIP, null, this.name, routing.RT_WAN_ROUTABLE, null, new Address4(allowedIP).isValid() ? 4 : 6).catch((err) => {});                
+              }
+            }            
+
             if (this.isWAN()) {
               // add peer networks to interface default routing table
               await routing.addRouteToTable(allowedIP, null, this.name, `${this.name}_default`, null, new Address4(allowedIP).isValid() ? 4 : 6).catch((err) => {});
